859c0e542a69f0cdb8b8b05b6bfe8ba837509979
Khalid Melouka <khalidmelouka@gmail.com>
2022-06-19 21:05:25 +0100

    added buttons, event listner and logic

--- script.js
+++ script.js
@@ -2,39 +2,48 @@
 // then creating the list of choices for the computer 
 // also variables for the player score and computer score
 
-let computerScore = 0; 
-let userScore = 0; 
-let computerChoice; 
-let userChoice; 
+
+const player = document.querySelector('#player'); 
+const machine = document.querySelector('#machine');
+const display = document.querySelector('.choice-display'); 
+let selectedButton;
+const buttons = document.querySelectorAll('button');
+
+let playerScore = 0; 
+let machineScore = 0; 
+let x = 0;
 let machineArray = ["rock", "paper", "scissors"]; 
-let result; // this will return a value of the singleRound func so i can use it the game loop
+
+let computerPlay = () => {
+    return machineArray[Math.floor(Math.random() * (machineArray.length))]; 
+};
 
 
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    selectedButton = button.className;
+    playRound(selectedButton, computerPlay());
+}));
 
-// this will randomly pick randomly from the array 
-function computerPlay() {
-    computerSelect = machineArray[Math.floor(Math.random() * (machineArray.length))]; 
-    return computerSelect; 
-} 
 
-function singleRound(userSelection, computerSelection) { 
-    if (userSelection === computerSelection) {
-        return "tie"; 
-    } else if ((userSelection === "rock" && computerSelection === "scissors") ||
-            (userSelection === "paper" && computerSelection === "rock") || 
-            (userSelection === "scissors") && computerSelection === "paper")
-    {
-        return "player"; 
-    } else if (((computerSelection === "rock" && userSelection === "scissors") ||
-            (computerSelection === "paper" && userSelection === "rock") || 
-            (computerSelection === "scissors") && userSelection === "paper")) {
-        return "machine"; 
-    } else {
-        alert("Something went wrong. Check your spellling");
-        alert(userSelection);
-    }
-
-} 
+function playRound(selection, computerChoice) {
+    if (selection === "rock" && computerChoice === "paper" ||
+        selection === "scissors" && computerChoice === "rock" || 
+        selection === "paper" && computerChoice === "scissors") {
+        display.textContent = `you chose ${selection} and machine chose ${computerChoice}, machine wins`;
+        x += 1;
+        machineScore += 1;
+        machine.textContent = machineScore;
+    } else if (selection === "paper" && computerChoice === "rock" ||
+        selection === "rock" && computerChoice === "scissors" || 
+        selection === "scissors" && computerChoice === "paper") {
+        display.textContent = `you chose ${selection} and machine chose ${computerChoice}, you win`;
+        playerScore += 1;
+        player.textContent = playerScore;
+    } else if (selection === computerChoice) {
+        display.textContent = `you chose ${selection} and machine chose ${computerChoice}, it's a tie`;
+        x += 1;
+    };
+};
 
 
 
